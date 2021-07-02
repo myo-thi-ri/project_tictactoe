b9a4294590d3e443970bce4cf919a94bcdb6425a
myo_thi_ri <sandarlamin.llm@gmail.com>
2021-07-02 13:42:51 +0900

    All functionality is added.

--- js/GameView.js
+++ js/GameView.js
@@ -3,13 +3,41 @@ export default class GameView {
     console.log("Init GameView.");
   }
   updateBoard(game) {
-    console.log("this is game view");
-    console.log(game.board);
+    // console.log("this is game view");
+    // console.log(game.board);
+    this.winningCombination = game.findWinningCombinations();
+    this.updateTurn(game);
     for (let i = 0; i < game.board.length; i++) {
       //   console.log(game.board[i]);
+
       const tile = document.querySelector(`.board-tile[data-index='${i}']`);
-      console.log(tile);
-      tile.textContent = game.board[i];
+      // console.log(tile);
+
+      tile.classList.remove("tile-winner");
+
+      let tileType = game.board[i] == "X" ? "title-x" : "tile-o";
+      // tile.textContent = game.board[i];
+      tile.innerHTML = `<span class="${tileType}">${
+        game.board[i] ? game.board[i] : ""
+      }</span>`;
+
+      if (this.winningCombination && this.winningCombination.includes(i)) {
+        tile.classList.add("tile-winner");
+      }
+    }
+  }
+
+  updateTurn(game) {
+    let playerX = document.querySelector(".player-X");
+    let playerO = document.querySelector(".player-O");
+    // console.log(playerO);
+    // console.log(playerX);
+    playerO.classList.remove("active");
+    playerX.classList.remove("active");
+    if (game.turn == "X") {
+      playerX.classList.add("active");
+    } else {
+      playerO.classList.add("active");
     }
   }
 }
--- js/script.js
+++ js/script.js
@@ -3,15 +3,39 @@ import GameView from "./GameView.js";
 
 let game = new Game();
 let gameView = new GameView();
-gameView.updateBoard(game);
-console.log(game.board);
-console.log("My turn is", game.turn);
-console.log("Hello");
-game.nextTurn();
-console.log("My turn is", game.turn);
-game.makeMove(2);
-console.log(game.board);
-game.nextTurn();
-game.makeMove(5);
-console.log(game.board);
+
+document.querySelector(".restart").addEventListener("click", () => {
+  onRestartClick();
+});
+// gameView.updateBoard(game);
+// console.log(game.board);
+// console.log("My turn is", game.turn);
+// console.log("Hello");
+// game.nextTurn();
+// console.log("My turn is", game.turn);
+// game.makeMove(2);
+// console.log(game.board);
+// game.nextTurn();
+// game.makeMove(5);
+// console.log(game.board);
+// gameView.updateBoard(game);
+
+let tiles = document.querySelectorAll(".board-tile");
+tiles.forEach((tile) => {
+  tile.addEventListener("click", () => {
+    onTileClick(tile.dataset.index);
+  });
+});
+
+function onTileClick(i) {
+  game.makeMove(i);
+  gameView.updateBoard(game);
+  game.nextTurn();
+}
+
+function onRestartClick() {
+  game = new Game();
+  gameView.updateBoard(game);
+}
+
 gameView.updateBoard(game);
